ad89548616fbde70b491504da7adc84f998b63eb
jorfsson <35546435+jorfsson@users.noreply.github.com>
2018-03-14 16:52:37 -0700

    Created CalendarTrigger on Event start

---
+++ caelndarTrigger-conditionals.js
@@ -0,0 +1,20 @@
+function calendarEventTrigger(){
+
+
+var today = new Date();
+var todayMin = today.getMinutes();
+var todayHr = today.getHours();
+var events = CalendarApp.getCalendarById('jeffreychea234@gmail.com').getEventsForDay(today);
+for (var i=0;i<events.length;i++) {
+var eventStart = events[i].getStartTime();
+var eventStartMin = eventStart.getMinutes();
+var eventStartHr = eventStart.getHours();
+Logger.log(eventStartMin);
+
+  if ((todayHr == eventStartHr) && ((Math.abs(todayMin - eventStartMin) <= 7)) && (Math.abs(todayMin - eventStartMin) >= -5)) {
+    lagProduct();
+  } else {
+    Logger.log("nolo wurld;");
+    }
+  }
+}
