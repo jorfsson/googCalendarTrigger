5e3cabb2ab44558f6f6033a64c89c87bc3d0ce83
jorfsson <35546435+jorfsson@users.noreply.github.com>
2018-03-14 17:12:42 -0700

    changed conditionals

--- caelndarTrigger-conditionals.js
+++ caelndarTrigger-conditionals.js
@@ -1,20 +1,16 @@
-function calendarEventTrigger(){
+function calendarTrigger(){
+  var today = new Date();
+  var todayMin = today.getMinutes();
+  var todayHr = today.getHours();
 
+  var events = CalendarApp.getCalendarById('jeffreychea234@gmail.com').getEventsForDay(today);
+  for (var i=0;i<events.length;i++) {
+    var eventStart = events[i].getStartTime();
+    var eventStartMin = eventStart.getMinutes();
+    var eventStartHr = eventStart.getHours();
 
-var today = new Date();
-var todayMin = today.getMinutes();
-var todayHr = today.getHours();
-var events = CalendarApp.getCalendarById('jeffreychea234@gmail.com').getEventsForDay(today);
-for (var i=0;i<events.length;i++) {
-var eventStart = events[i].getStartTime();
-var eventStartMin = eventStart.getMinutes();
-var eventStartHr = eventStart.getHours();
-Logger.log(eventStartMin);
-
-  if ((todayHr == eventStartHr) && ((Math.abs(todayMin - eventStartMin) <= 7)) && (Math.abs(todayMin - eventStartMin) >= -5)) {
-    lagProduct();
-  } else {
-    Logger.log("nolo wurld;");
+    if ((todayHr == eventStartHr) && ((Math.abs(todayMin - eventStartMin) <= 5) && (Math.abs(todayMin - eventStartMin) >= -5))) {
+      lagProduct();
     }
   }
-}
+} 
